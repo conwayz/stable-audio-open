3327b8e6d012c178fad2d29eaf483cf4c03d8d5e
conway <conwayzhu@gmail.com>
2024-07-11 22:27:46 -0700

    pretransform/conditioner; factory

---
+++ factory.py
@@ -0,0 +1,148 @@
+"""
+Creating different components of the model from config.
+"""
+
+import typing as tp
+from typing import Any, Dict
+
+from model import (
+    AudioAutoencoder,
+    AutoencoderPretransform,
+    MultiConditioner,
+    NumberConditioner,
+    OobleckDecoder,
+    T5Conditioner,
+)
+
+"""
+Conditioner
+"""
+
+
+def create_multi_conditioner_from_conditioning_config(
+    config: tp.Dict[str, tp.Any]
+) -> MultiConditioner:
+    """
+    Create a MultiConditioner from a conditioning config dictionary
+
+    Args:
+        config: the conditioning config dictionary
+        device: the device to put the conditioners on
+    """
+    conditioners = {}
+    cond_dim = config["cond_dim"]
+
+    default_keys = config.get("default_keys", {})
+
+    for conditioner_info in config["configs"]:
+        id = conditioner_info["id"]
+
+        conditioner_type = conditioner_info["type"]
+
+        conditioner_config = {"output_dim": cond_dim}
+
+        conditioner_config.update(conditioner_info["config"])
+
+        if conditioner_type == "t5":
+            conditioners[id] = T5Conditioner(**conditioner_config)
+        elif conditioner_type == "number":
+            conditioners[id] = NumberConditioner(**conditioner_config)
+        else:
+            raise ValueError(f"Unknown conditioner type: {conditioner_type}")
+
+    return MultiConditioner(conditioners, default_keys=default_keys)
+
+
+"""
+Autoencoder/Pretransform
+"""
+
+
+def create_autoencoder_from_config(config: Dict[str, Any]):
+
+    ae_config = config["model"]
+
+    decoder = create_decoder_from_config(ae_config["decoder"])
+
+    latent_dim = ae_config.get("latent_dim", None)
+    assert latent_dim is not None, "latent_dim must be specified in model config"
+    downsampling_ratio = ae_config.get("downsampling_ratio", None)
+    assert (
+        downsampling_ratio is not None
+    ), "downsampling_ratio must be specified in model config"
+    io_channels = ae_config.get("io_channels", None)
+    assert io_channels is not None, "io_channels must be specified in model config"
+    sample_rate = config.get("sample_rate", None)
+    assert sample_rate is not None, "sample_rate must be specified in model config"
+
+    in_channels = ae_config.get("in_channels", None)
+    out_channels = ae_config.get("out_channels", None)
+
+    return AudioAutoencoder(
+        None,
+        decoder,
+        io_channels=io_channels,
+        latent_dim=latent_dim,
+        downsampling_ratio=downsampling_ratio,
+        sample_rate=sample_rate,
+        bottleneck=None,
+        pretransform=None,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        soft_clip=False,
+    )
+
+
+def create_decoder_from_config(decoder_config: Dict[str, Any]):
+    decoder_type = decoder_config.get("type", None)
+    assert decoder_type is not None, "Decoder type must be specified"
+
+    if decoder_type == "oobleck":
+        decoder = OobleckDecoder(**decoder_config["config"])
+    else:
+        raise ValueError(f"Unknown decoder type {decoder_type}")
+
+    requires_grad = decoder_config.get("requires_grad", True)
+    if not requires_grad:
+        for param in decoder.parameters():
+            param.requires_grad = False
+
+    return decoder
+
+
+def create_pretransform_from_config(pretransform_config, sample_rate):
+    pretransform_type = pretransform_config.get("type", None)
+
+    assert (
+        pretransform_type is not None
+    ), "type must be specified in pretransform config"
+
+    if pretransform_type == "autoencoder":
+        # Create fake top-level config to pass sample rate to autoencoder constructor
+        # This is a bit of a hack but it keeps us from re-defining the sample rate in the config
+        autoencoder_config = {
+            "sample_rate": sample_rate,
+            "model": pretransform_config["config"],
+        }
+        autoencoder = create_autoencoder_from_config(autoencoder_config)
+
+        scale = pretransform_config.get("scale", 1.0)
+        model_half = pretransform_config.get("model_half", False)
+        iterate_batch = pretransform_config.get("iterate_batch", False)
+
+        pretransform = AutoencoderPretransform(
+            autoencoder,
+            scale=scale,
+            model_half=model_half,
+            iterate_batch=iterate_batch,
+            chunked=False,
+        )
+    else:
+        raise NotImplementedError(f"Unknown pretransform type: {pretransform_type}")
+
+    enable_grad = pretransform_config.get("enable_grad", False)
+    pretransform.enable_grad = enable_grad
+
+    pretransform.eval().requires_grad_(pretransform.enable_grad)
+
+    return pretransform
